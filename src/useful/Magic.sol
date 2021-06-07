147e04a83d1e03c3d31094c139a2be2a902ca714
ivan kotelnikov <ivan.d.kotelnikov@gmail.com>
2021-06-08 02:07:01 +0300

    ~

--- src/useful/Magic.sol
+++
@@ -1,9 +0,0 @@
-contract Safe {
-   constructor() public {
-        TvmCell code = tvm.code();
-        optional(TvmCell) salt = tvm.codeSalt(code);
-        require(salt.hasValue(), 101);
-        (, address rootAddr) = salt.get().toSlice().decode(uint8, address);
-        require(msg.sender == rootAddr, 102);
-    }
-}
